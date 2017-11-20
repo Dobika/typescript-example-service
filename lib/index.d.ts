a0978cdb9c823eae1608cb1073ada5ea7460b229
Janos Dobronszki <dobronszki@gmail.com>
2017-11-20 18:45:27 +0100

    Build again

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="express" />
+import * as express from 'express';
+export declare function Hi(req: express.Request, rsp: express.Response): void;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
 
-function Hi(req: express.Request, rsp: express.Response) {
+export function Hi(req: express.Request, rsp: express.Response) {
     rsp.send("Hi, this is coming from the example typescript service.")
 }
